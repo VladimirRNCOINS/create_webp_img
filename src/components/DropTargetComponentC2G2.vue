0f1802a673478594522238d54a24dd394b142054
VladimirRNCOINS <v.shabalov@rncoins.ru>
2024-10-29 13:35:39 +0300

    весь код по загрузке файлов в mixin loadFile

--- src/components/DropTargetComponentC2G2.vue
+++ src/components/DropTargetComponentC2G2.vue
@@ -33,29 +33,6 @@
     export default {
         name: 'DropTargetComponentC2G2',
         methods: {
-            loadFile(event) {
-                var input = event.target
-                var wrapInput = input.parentNode
-                var imgWrapElement = wrapInput.nextElementSibling
-                var imgLoaded = imgWrapElement.querySelector(".loadImageResult")
-                imgLoaded.src = URL.createObjectURL(event.target.files[0])
-                imgLoaded.onload = () => {
-                    imgLoaded.removeAttribute('width')
-                    imgLoaded.removeAttribute('height')
-                    if (imgLoaded.naturalWidth > imgLoaded.naturalHeight) {
-                        imgLoaded.style.removeProperty("height")
-                        imgLoaded.style.width = 88 + '%'
-                    }
-                    else if (imgLoaded.naturalWidth < imgLoaded.naturalHeight) {
-                        imgLoaded.style.removeProperty("width")
-                        imgLoaded.style.height = 88 + '%'
-                    }
-                    else {
-                        imgLoaded.style.removeProperty("height")
-                        imgLoaded.style.width = 88 + '%'
-                    }
-                }
-            },
             overLeftFile(event) {
                 event.stopPropagation()
                 if (!event.currentTarget.classList.contains('face_coin_magenta')) {
--- src/mixins/loadFile.js
+++ src/mixins/loadFile.js
@@ -6,6 +6,22 @@ export default {
             var imgWrapElement = wrapInput.nextElementSibling
             var imgLoaded = imgWrapElement.querySelector(".loadImageResult")
             imgLoaded.src = URL.createObjectURL(event.target.files[0])
+            imgLoaded.onload = () => {
+                imgLoaded.removeAttribute('width')
+                imgLoaded.removeAttribute('height')
+                if (imgLoaded.naturalWidth > imgLoaded.naturalHeight) {
+                    imgLoaded.style.removeProperty("height")
+                    imgLoaded.style.width = 88 + '%'
+                }
+                else if (imgLoaded.naturalWidth < imgLoaded.naturalHeight) {
+                    imgLoaded.style.removeProperty("width")
+                    imgLoaded.style.height = 88 + '%'
+                }
+                else {
+                    imgLoaded.style.removeProperty("height")
+                    imgLoaded.style.width = 88 + '%'
+                }
+            }
         },
     }
 }
